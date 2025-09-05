46348f46545127038e0f3e1b64a8e144c0817208
biruk habte <biruk.habte-ug@aau.edu.et>
2025-09-06 02:19:04 +0300

    Update rental contract templates

--- Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/edit-vehicle.php
+++ Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/edit-vehicle.php
@@ -491,3 +491,4 @@ echo htmlentities("File not available");
 </html>
 <?php } ?>// Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
+// Updated: Fri Mar 27 10:25:13 AM EAT 2026
