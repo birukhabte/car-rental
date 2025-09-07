4199dc5ecd3a5fa1796ba9c724a52ae6c34fa601
biruk habte <biruk.habte-ug@aau.edu.et>
2025-09-08 01:35:36 +0300

    Add vehicle fleet analytics

--- Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/create-brand.php
+++ Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/create-brand.php
@@ -154,3 +154,4 @@ $error="Something went wrong. Please try again";
 <?php } ?>// Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
+// Updated: Fri Mar 27 10:25:13 AM EAT 2026
