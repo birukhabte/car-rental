a583865cb84a4a11cd1cf37160953db23b807150
biruk habte <biruk.habte-ug@aau.edu.et>
2025-09-11 15:06:52 +0300

    Add vehicle GPS tracking feature

--- Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/dashboard.php
+++ Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/dashboard.php
@@ -255,3 +255,4 @@ $testimonials=$query5->rowCount();
 </html>
 <?php } ?>// Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
+// Updated: Fri Mar 27 10:25:13 AM EAT 2026
