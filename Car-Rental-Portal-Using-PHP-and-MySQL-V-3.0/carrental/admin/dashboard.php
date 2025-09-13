f16678893b39b794416c5e60cc86de02395e535f
biruk habte <biruk.habte-ug@aau.edu.et>
2025-09-13 21:41:03 +0300

    Fix rental agreement PDF generation

--- Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/dashboard.php
+++ Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/dashboard.php
@@ -256,3 +256,4 @@ $testimonials=$query5->rowCount();
 <?php } ?>// Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
+// Updated: Fri Mar 27 10:25:13 AM EAT 2026
