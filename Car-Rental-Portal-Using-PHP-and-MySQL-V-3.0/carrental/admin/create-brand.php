d7112848c02ee2a9130ef7d74ded103f34cf936c
biruk habte <biruk.habte-ug@aau.edu.et>
2025-09-15 01:47:49 +0300

    Improve vehicle image upload validation

--- Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/create-brand.php
+++ Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/create-brand.php
@@ -156,3 +156,4 @@ $error="Something went wrong. Please try again";
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
+// Updated: Fri Mar 27 10:25:13 AM EAT 2026
