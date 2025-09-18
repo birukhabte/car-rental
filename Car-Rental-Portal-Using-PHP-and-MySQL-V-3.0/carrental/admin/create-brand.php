9f3ef51d068f13dfc8a4e328fb47604988eceff5
biruk habte <biruk.habte-ug@aau.edu.et>
2025-09-18 09:18:54 +0300

    Fix customer profile update issue

--- Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/create-brand.php
+++ Car-Rental-Portal-Using-PHP-and-MySQL-V-3.0/carrental/admin/create-brand.php
@@ -157,3 +157,4 @@ $error="Something went wrong. Please try again";
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
 // Updated: Fri Mar 27 10:25:13 AM EAT 2026
+// Updated: Fri Mar 27 10:25:14 AM EAT 2026
